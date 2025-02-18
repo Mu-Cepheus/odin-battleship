42b116deab948c76c05e091144a40c298714e30b
Mu-Cepheus <>
2025-02-19 00:47:02 +0800

    refactor: tests use setup and teardown functions

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,49 +1,47 @@
 import Gameboard from "./../scripts/gameboard.js";
 
+let gb;
+
+beforeEach(() => {
+  gb = new Gameboard();
+  gb.createShip("carrier", [0, 0], "down");
+  gb.createShip("battleship", [0, 1], "down");
+  gb.createShip("destroyer", [0, 2], "down");
+  gb.createShip("submarine", [0, 3], "down");
+});
+
+afterEach(() => {
+  gb = null;
+});
+
 test("Gameboard constructor creates boards", () => {
-  let gb = new Gameboard();
   expect(gb.grid.length).toBe(10);
 });
 
 test("Create a ship and report", () => {
-  let gb = new Gameboard();
-  expect(gb.createShip("carrier", [0, 0], "down")).toBe(true);
   expect(gb.createShip("patrolBoat", [9, 1], "left")).toBe(true);
 });
 
 test("Prevent from creating same ship twice", () => {
-  let gb = new Gameboard();
-  gb.createShip("carrier", [0, 0], "down");
   expect(() => gb.createShip("carrier", [0, 5], "right")).toThrow(
     "instantiated",
   );
 });
 
 test("Prevent stacking ships on squares", function () {
-  let gb = new Gameboard();
-  gb.createShip("carrier", [0, 0], "down");
   expect(() => gb.createShip("patrolBoat", [0, 1], "left")).toThrow("overlaps");
 });
 
 test("Hitting ships", () => {
-  let gb = new Gameboard();
-  gb.createShip("carrier", [0, 0], "down");
-  expect(gb.receiveAttack(0, 1)).toBe(true); //miss a ship
+  expect(gb.receiveAttack(9, 9)).toBe(true); //miss a ship
   expect(gb.receiveAttack(1, 0)).toBe(1); //hit it
 });
 
 test("Hitting empty squares twice", () => {
-  let gb = new Gameboard();
-  gb.createShip("carrier", [0, 0], "down");
-  expect(gb.receiveAttack(0, 1)).toBe(true); //miss a ship
-  expect(() => gb.receiveAttack(0, 1)).toThrow("already");
+  expect(gb.receiveAttack(9, 9)).toBe(true); //miss a ship
+  expect(() => gb.receiveAttack(9, 9)).toThrow("already");
 });
 
 test("Reports if all ships have sunk", () => {
-  let gb = new Gameboard();
-  gb.createShip("carrier", [0, 0], "down");
-  gb.createShip("battleship", [0, 1], "down");
-  gb.createShip("destroyer", [0, 2], "down");
-  gb.createShip("submarine", [0, 3], "down");
   gb.createShip("patrolBoat", [0, 4], "down");
 });
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,19 +1,24 @@
 import Ship from "./../scripts/ship.js";
 
-test("Check hit return value", () => {
-  let bro = new Ship(2, [
+let bro;
+
+beforeEach(() => {
+  bro = new Ship(2, [
     [0, 0],
     [0, 1],
   ]);
+});
+
+afterEach(() => {
+  bro = null;
+});
+
+test("Check hit return value", () => {
   expect(bro.hit()).toBe(1);
   expect(bro.hit()).toBe(2);
 });
 
 test("Check sink return values", () => {
-  let bro = new Ship(2, [
-    [0, 0],
-    [0, 1],
-  ]);
   expect(bro.isSunk()).toBe(false);
   bro.hit();
   bro.hit();
@@ -21,10 +26,6 @@ test("Check sink return values", () => {
 });
 
 test("Check location reporting", () => {
-  let bro = new Ship(2, [
-    [0, 0],
-    [0, 1],
-  ]);
   expect(bro.coords).toStrictEqual([
     [0, 0],
     [0, 1],
